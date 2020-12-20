3450b89d8aea4a6b9bf3990d85192d374f4937c5
Alex Bachicha <alexandra.bachicha@hotmail.com>
2020-12-19 18:28:48 -0800

    cfixed spacing of code

--- routes/apiroute.js
+++ routes/apiroute.js
@@ -5,23 +5,20 @@ var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 module.exports = function(app) {
 
     app.get("/api/notes", function(req, res) {
-       
         res.json(data);
-
     });
 
     app.get("/api/notes/:id", function(req, res) {
-
         res.json(data[Number(req.params.id)]);
-
     });
 
 
     app.post("/api/notes", function(req, res) {
-
         let newNote = req.body;
         let uniqueId = (data.length).toString();
+
         console.log(uniqueId);
+
         newNote.id = uniqueId;
         data.push(newNote);
         
@@ -30,15 +27,15 @@ module.exports = function(app) {
         }); 
 
         res.json(data);    
-
     });
 
     
     app.delete("/api/notes/:id", function(req, res) {
-
         let noteId = req.params.id;
         let newId = 0;
+
         console.log(`Deleting note with id ${noteId}`);
+
         data = data.filter(currentNote => {
            return currentNote.id != noteId;
         });
@@ -46,6 +43,7 @@ module.exports = function(app) {
             currentNote.id = newId.toString();
             newId++;
         }
+        
         fs.writeFileSync("./db/db.json", JSON.stringify(data));
         res.json(data);
     }); 
